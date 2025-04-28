538b708067e664408cde20581943a1fe8ca734da
Gabriel <gabrielh.oliveira222@gmail.com>
2025-04-28 20:41:02 -0300

    ✨ feat(core): Rename agent builder functions for clarity and add director agent builder

--- src/commonMain/kotlin/dev/gabrielolv/kaia/core/KAIAgentSystemHelpers.kt
+++ src/commonMain/kotlin/dev/gabrielolv/kaia/core/KAIAgentSystemHelpers.kt
@@ -3,10 +3,14 @@ package dev.gabrielolv.kaia.core
 import dev.gabrielolv.kaia.core.agents.*
 
 
-fun KAIAgentSystemBuilder.llm(block: (LLMAgentBuilder.() -> Unit)) {
+fun KAIAgentSystemBuilder.buildLLMAgent(block: (LLMAgentBuilder.() -> Unit)) {
     this.addAgent(Agent.llm(block))
 }
 
-fun KAIAgentSystemBuilder.database(block: (DatabaseAgentBuilder.() -> Unit)) {
+fun KAIAgentSystemBuilder.buildDatabaseAgent(block: (DatabaseAgentBuilder.() -> Unit)) {
     this.addAgent(Agent.database(block))
+}
+
+fun KAIAgentSystemBuilder.buildDirectorAgent(block: (DirectorAgentBuilder.() -> Unit)) {
+    this.addAgent(Agent.director(block))
 }
