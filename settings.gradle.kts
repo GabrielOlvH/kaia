0d1da476b190037d8b62a2f26e452d8416ebedee
Gabriel <gabrielh.oliveira222@gmail.com>
2025-04-30 14:30:19 -0300

    🔧 fix(build): Temp disable mingw

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlin/native/dev/") }
+        // Repository for Kotlin/Native prebuilt distributions - REMOVED
+        // maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlin/native/dev/") }
     }
 }
 
